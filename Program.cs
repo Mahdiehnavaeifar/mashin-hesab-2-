39b252ad74921ea24be246875ecc5100ab24e9cb
Mahdiehnavaeifar <126653389+Mahdiehnavaeifar@users.noreply.github.com>
2023-05-05 18:08:30 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//mahdieh navaeifar
+
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the operation (+, -, *, /): ");
+            string operation = Console.ReadLine();
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation!");
+                    break;
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
+        }
+    }
+}
+
+
